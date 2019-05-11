d506635ee8c847dfad6804fe99f241fc3520bbef
Patrick Braunstorfer <patrick.braunstorfer@outlook.com>
2019-05-11 22:39:37 +0200

    Project creation.

---
+++ src/RainbowsForBlood.TabletopTools/RainbowsForBlood.TabletopTools.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RainbowsForBlood.TabletopTools.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/RainbowsForBlood.TabletopTools/RainbowsForBlood.TabletopTools.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RainbowsForBlood.TabletopTools.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
